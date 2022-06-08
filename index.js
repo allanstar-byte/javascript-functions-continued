46e6d6abbbc992dc87bfbc929b11472f1b71eec1
allanstar-byte <allanmutisya263@gmail.com>
2022-06-08 04:49:36 +0300

    Funtions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(string = "roller-skate") {
+    console.log(`This Saturday, I want to ${string}!`);
+};
+
+saturdayFun()
+
+const mondayWork = function(work = "go to the office") {
+    console.log(`This Monday, I want to ${work}`);
+};
+mondayWork('bathe my dog!');
+
+
+function wrapAdjective(quo = '*') {
+    return function(status = "special") {
+        return `You are ${quo} ${status} ${quo}!`
+    }
+}
+wrapAdjective('||')('funny');
